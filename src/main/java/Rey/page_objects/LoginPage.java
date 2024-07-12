9097a66aedb17d6fd6faaed53337441c102f78b2
Alfaza Satria <alfazasatria8@gmail.com>
2024-07-12 13:26:37 +0700

    Commit

--- src/main/java/Rey/page_objects/LoginPage.java
+++ src/main/java/Rey/page_objects/LoginPage.java
@@ -1,2 +1,22 @@
-package Rey.page_objects;public class LoginPage {
+package Rey.page_objects;
+
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"No. Handphone/Email/Username\")")
+    protected WebElement usernameField;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Password\")")
+    protected WebElement passwordField;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.shopee.id.int:id/btnLogin\")")
+    protected WebElement loginButton;
+
+    public void login(String username, String password) {
+        usernameField.sendKeys(username);
+        passwordField.sendKeys(password);
+        loginButton.click();
+    }
 }
--- src/main/java/Rey/page_objects/MePage.java
+++ src/main/java/Rey/page_objects/MePage.java
@@ -1,2 +1,20 @@
-package Rey.page_objects;public class MePage {
+package Rey.page_objects;
+
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
+
+public class MePage {
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"buttonSignup\")")
+    protected WebElement signUpButton;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().className(\"android.view.ViewGroup\").instance(101)")
+    protected WebElement signInButton;
+
+    public void clickSignUpButton() {
+        signUpButton.click();
+    }
+
+    public void clickSignInButton() {
+        signInButton.click();
+    }
 }
